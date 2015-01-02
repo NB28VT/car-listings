9297f4b76b422fdaf057212bf84228ac739acd31
Nathan Burgess <nathan.burgess28@hotmail.com>
2015-01-02 10:25:47 -0500

    Added unit testing to manufacturer model. Manufacturer feature complete

--- spec/models/manufacturer_spec.rb
+++ spec/models/manufacturer_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Manufacturer, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "allows a user to enter a valid manufacturer" do
+    manufacturer = Manufacturer.new
+
+    manufacturer.name = "Porsche"
+    manufacturer.country = "Germany"
+
+    expect(manufacturer.save).to eq(true)
+  end
+
+  it "does not allow a user to enter a blank name" do
+    manufacturer = Manufacturer.new
+
+    manufacturer.country = "Germany"
+
+    expect(manufacturer.save).to eq(false)
+  end
+
+  it "does not allow a user to enter a blank country" do
+    manufacturer = Manufacturer.new
+
+    manufacturer.name = "Porsche"
+
+    expect(manufacturer.save).to eq(false)
+  end
+
 end
